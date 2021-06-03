0584b403c30c72b0e511eed13af3768a6f8aaa76
Ryan Verzo <ryanemmanuelvrz345@gmail.com>
2021-06-03 14:15:06 +0800

    initial routes

--- CQL/src/app/app-routing.module.ts
+++ CQL/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EdgeComponent } from './components/edge/edge.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeopleComponent } from './components/people/people.component';
+import { ServicesComponent } from './components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'edge', component: EdgeComponent },
+  { path: 'clients', component: ClientsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- CQL/src/app/app.module.ts
+++ CQL/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeopleComponent } from './components/people/people.component';
+import { ServicesComponent } from './components/services/services.component';
+import { EdgeComponent } from './components/edge/edge.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PeopleComponent,
+    ServicesComponent,
+    EdgeComponent,
+    ClientsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
